493d2a1da37a7413a7945868ef9d405a07dee7d0
syhhyl <syhhyl926@126.com>
2026-05-05 11:46:16 +0800

    add build type release

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 set -e
 
-cmake -S . -B build
+build_type=${1:-debug}
+
+case "$build_type" in
+  debug|Debug)
+    cmake_build_type=Debug
+    ;;
+  release|Release)
+    cmake_build_type=Release
+    ;;
+  *)
+    echo "usage: ./build.sh [debug|release]" >&2
+    exit 1
+    ;;
+esac
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE="$cmake_build_type"
 cmake --build build
